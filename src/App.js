f3d7f96a76cd24e83867da07f53af2612eec7e5a
Vincent Kioko <vincentkioko51@gmail.com>
2023-07-11 16:43:16 +0300

    Component Composition
    React Component Composition

--- src/App.js
+++ src/App.js
@@ -1,81 +1,92 @@
 import * as React from 'react'
 
+const useSemiPersistentState = (key, initialState) => {
+	/**React State */
+	const [value, setValue] = React.useState(
+		localStorage.getItem(key) || initialState
+	)
+
+	React.useEffect(() => {
+		localStorage.setItem(key, value)
+	}, [value, key])
+
+	return [value, setValue]
+}
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1
-    }
-  ];
+	const stories = [
+		{
+			title: 'React',
+			url: 'https://reactjs.org/',
+			author: 'Jordan Walke',
+			num_comments: 3,
+			points: 4,
+			objectID: 0
+		},
+		{
+			title: 'Redux',
+			url: 'https://redux.js.org/',
+			author: 'Dan Abramov, Andrew Clark',
+			num_comments: 2,
+			points: 5,
+			objectID: 1
+		}
+	];
 
-  /**React State */
-  const [searchTerm, setSearchTerm] = React.useState('')
+	/**Custom State */
+	const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React')
+	
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value)
+	}
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value)
-  }
+	const searchedStories = stories.filter((story)=>{
+		return story.title.toLowerCase().includes(searchTerm.toLowerCase())
+	})
+	
 
-  const searchedStories = stories.filter((story)=>{
-    return story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  })
-  
-  console.log('App Renders!');
-
-  return (
-    <div>
-      <h1>Hacker Stories</h1>
-      <Search onSearch={handleSearch} />
-      <hr />
-      {/* rendering the list */}
-      <List list={searchedStories} />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hacker Stories</h1>
+			<InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch}>
+				<strong>Search: </strong>
+			</InputWithLabel>
+			<hr />
+			{/* rendering the list */}
+			<List list={searchedStories} />
+		</div>
+	);
 }
 
-const Search = (props) => {
-  console.log('Search Renders!')
-  return (
-    <div>
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' onChange={props.onSearch} />
-    </div>
-  )
-}
+const InputWithLabel = ({id, value, type='text', onInputChange, children,}) => (
+	<>
+		<label htmlFor={id}>{children}</label>&nbsp;
+		<input 
+			id={id} 
+			type={type} 
+			value={value} 
+			onChange={onInputChange} 
+		/>
+	</>
+)
 
-const List = (props) => {
-  console.log('List Renders!')
+const List = ({list}) => (
+	<ul>
+		{list.map(({objectID, ...item}) => (
+			<Item key={objectID} {...item} />
+		))}
+	</ul>
+)
 
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-  );
-}
-
-
-const Item = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
+const Item = ({title, url, author, num_comments, points}) => (
+	<li>
+		<span>
+			<a href={url}>{title}</a>
+		</span>&nbsp;
+		<span>{author}</span>
+		<span>{num_comments}</span>
+		<span>{points}</span>
+	</li>
 )
 
 export default App;
