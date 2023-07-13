86ae46dd6257a222241edc427432fac8bdd8d93a
Vincent Kioko <vincentkioko51@gmail.com>
2023-07-13 17:36:05 +0300

    Inline Handlers in JSX

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const useSemiPersistentState = (key, initialState) => {
 }
 
 const App = () => {
-	const stories = [
+	const initialStories = [
 		{
 			title: 'React',
 			url: 'https://reactjs.org/',
@@ -35,6 +35,16 @@ const App = () => {
 
 	/**Custom State */
 	const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React')
+
+	const [stories, setStories] = React.useState(initialStories)
+
+	const handleRemoveStory = (item) => {
+		const newStories = stories.filter(
+			(story) => item.objectID !== story.objectID
+		);
+
+		setStories(newStories)
+	}
 	
 	const handleSearch = (event) => {
 		setSearchTerm(event.target.value)
@@ -57,7 +67,7 @@ const App = () => {
 			</InputWithLabel>
 			<hr />
 			{/* rendering the list */}
-			<List list={searchedStories} />
+			<List list={searchedStories} onRemoveItem={handleRemoveStory} />
 		</div>
 	);
 }
@@ -85,22 +95,31 @@ const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, child
 }
 
 
-const List = ({list}) => (
+const List = ({list, onRemoveItem}) => (
 	<ul>
-		{list.map(({objectID, ...item}) => (
-			<Item key={objectID} {...item} />
+		{list.map((item) => (
+			<Item
+				key={item.objectID}
+				item={item}
+				onRemoveItem={onRemoveItem}
+			/>
 		))}
 	</ul>
 )
 
-const Item = ({title, url, author, num_comments, points}) => (
+const Item = ({item, onRemoveItem}) => (
 	<li>
 		<span>
-			<a href={url}>{title}</a>
+			<a href={item.url}>{item.title}</a>
 		</span>&nbsp;
-		<span>{author}</span>
-		<span>{num_comments}</span>
-		<span>{points}</span>
+		<span>{item.author}</span>&nbsp;
+		<span>{item.num_comments}</span>&nbsp;
+		<span>{item.points}</span>&nbsp;
+		<span>
+			<button type="button" onClick={() => onRemoveItem(item)}>
+				Dismiss
+			</button>
+		</span>
 	</li>
 )
 
