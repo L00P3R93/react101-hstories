8a22a642ace86d5f1544cd9dbbefac9dc5e773ac
Vincent Kioko <vincentkioko51@gmail.com>
2023-07-17 17:15:26 +0300

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -41,16 +41,22 @@ const App = () => {
             )
         )
 
-    React.useEffect(() => {
-        getAsyncStories().then(result => {
-            setStories(result.data.stories)
-        })
-    })
+
 
 	/**Custom State */
 	const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React')
 
-	const [stories, setStories] = React.useState(initialStories)
+	const [stories, setStories] = React.useState([])
+	const [isLoading, setIsLoading] = React.useState(false)
+	const [isError, setIsError] = React.useState(false)
+
+	React.useEffect(() => {
+		setIsLoading(true)
+		getAsyncStories().then((result) => {
+			setStories(result.data.stories)
+			setIsLoading(false)
+		}).catch(() => setIsError(true))
+	})
 
 	const handleRemoveStory = (item) => {
 		const newStories = stories.filter(
@@ -80,8 +86,14 @@ const App = () => {
 				<strong>Search: </strong>
 			</InputWithLabel>
 			<hr />
-			{/* rendering the list */}
-			<List list={searchedStories} onRemoveItem={handleRemoveStory} />
+			{/** -- Conditional Rendering JSX
+			 true && 'Hello World' => 'Hello World',
+			 false && 'Hello World' => false,
+			 */}
+			{isError && <p>Something went wrong ...</p>}
+			{isLoading ? (<p>Loading ...</p>) : (
+				<List list={searchedStories} onRemoveItem={handleRemoveStory} />
+			)}
 		</div>
 	);
 }
