136167ec9f3f1d20c96e4f518ba8b481915247c9
Vincent Kioko <vincentkioko51@gmail.com>
2023-07-19 11:35:44 +0300

    Data Fetching & Re-Fetching

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import * as React from 'react'
 
-const initialStories = [
-	{
-		title: 'React',
-		url: 'https://reactjs.org/',
-		author: 'Jordan Walke',
-		num_comments: 3,
-		points: 4,
-		objectID: 0
-	},
-	{
-		title: 'Redux',
-		url: 'https://redux.js.org/',
-		author: 'Dan Abramov, Andrew Clark',
-		num_comments: 2,
-		points: 5,
-		objectID: 1
-	}
-];
+const API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?query='
 
-const getAsyncStories = () =>
-	new Promise((resolve, reject) => /*setTimeout(reject, 2000)*/
-		setTimeout(
-			() => resolve({data: {stories: initialStories}}),
-			2000
-		)
-	)
 
 const useSemiPersistentState = (key, initialState) => {
 	/**React State */
@@ -82,16 +58,20 @@ const App = () => {
 	)
 
 	React.useEffect(() => {
+		if(!searchTerm) return;
+
 		dispatchStories({ type: 'STORIES_FETCH_INIT' })
-		getAsyncStories().then((result) => {
+
+		fetch(`${API_ENDPOINT}${searchTerm}`).then((response) => response.json())
+		.then((result) => {
 			dispatchStories({
 				type: 'STORIES_FETCH_SUCCESS',
-				payload: result.data.stories,
+				payload: result.hits,
 			})
 		}).catch(
 			() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
 		)
-	}, [])
+	}, [searchTerm])
 
 	const handleRemoveStory = (item) => {
 		dispatchStories({
@@ -104,9 +84,9 @@ const App = () => {
 		setSearchTerm(event.target.value)
 	}
 
-	const searchedStories = stories.data.filter((story)=>{
+	/*const searchedStories = stories.data.filter((story)=>{
 		return story.title.toLowerCase().includes(searchTerm.toLowerCase())
-	})
+	})*/
 	
 
 	return (
@@ -126,7 +106,7 @@ const App = () => {
 			 */}
 			{stories.isError && <p>Something went wrong ...</p>}
 			{stories.isLoading ? (<p>Loading ...</p>) : (
-				<List list={searchedStories} onRemoveItem={handleRemoveStory} />
+				<List list={stories.data} onRemoveItem={handleRemoveStory} />
 			)}
 		</div>
 	);
