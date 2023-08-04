dfa9a7d3d7513c2ffad0a3d1eae84e72dd7c5569
Vincent Kioko <vincentkioko51@gmail.com>
2023-08-04 17:38:33 +0300

    Typescript
    Updated with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,65 @@ import styles from './App.module.css'
 
 import {ReactComponent as Check} from './check.svg'
 
+type Story = {
+	objectID: string;
+	url: string;
+	title: string;
+	author: string;
+	num_comments: number;
+	points: number;
+}
+
+type ItemProps = {
+	item: Story;
+	onRemoveItem: (item: Story) => void;
+}
+
+type Stories = Array<Story>
+
+type ListProps = {
+	list: Stories;
+	onRemoveItem: (item: Story) => void;
+}
+
+type StoriesState = {
+	data: Stories;
+	isLoading: boolean;
+	isError: boolean;
+}
+
+
+interface StoriesFetchInitAction {
+	type: 'STORIES_FETCH_INIT';
+}
+
+interface StoriesFetchSuccessAction {
+	type: 'STORIES_FETCH_SUCCESS';
+	payload: Stories;
+}
+
+interface StoriesFetchFailureAction {
+	type: 'STORIES_FETCH_FAILURE'
+}
+
+interface StoriesRemoveAction {
+	type: 'REMOVE_STORY';
+	payload: Story;
+}
+
+type StoriesAction = 
+	| StoriesFetchInitAction
+	| StoriesFetchSuccessAction
+	| StoriesFetchFailureAction
+	| StoriesRemoveAction
+
 const API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?query='
 
 
-const useSemiPersistentState = (key, initialState) => {
+const useSemiPersistentState = (
+	key: string, 
+	initialState: string
+): [string, (newValue: string) => void] => {
 	/**React State */
 	const [value, setValue] = React.useState(
 		localStorage.getItem(key) || initialState
@@ -23,7 +78,7 @@ const useSemiPersistentState = (key, initialState) => {
 	return [value, setValue]
 }
 
-const storiesReducer = (state, action) => {
+const storiesReducer = (state: StoriesState, action: StoriesAction) => {
 	switch(action.type) {
 		case 'STORIES_FETCH_INIT':
 			return {
@@ -56,14 +111,6 @@ const storiesReducer = (state, action) => {
 	}
 }
 
-const getSumComments = (stories) => {
-	console.log('C')
-	return stories.data.reduce(
-		(result, value) => result + value.num_comments,
-		0
-	)
-}
-
 const App = () => {
 	/**Custom State */
 	const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React')
@@ -91,26 +138,25 @@ const App = () => {
 		handleFetchStories()
 	}, [handleFetchStories]);
 
-	const handleRemoveStory = (item) => {
+	const handleRemoveStory = (item: Story) => {
 		dispatchStories({
 			type: 'REMOVE_STORY',
 			payload: item
 		})
 	}
 	
-	const handleSearchInput = (event) => {
+	const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchTerm(event.target.value)
 	}
-	const handleSearchSubmit = (event) => {
+	const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		setUrl(`${API_ENDPOINT}${searchTerm}`)
 		event.preventDefault();
 	}
 
-	const sumComments = React.useMemo(() => getSumComments(stories), [stories,]) 
 
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.headlinePrimary}>Hacker Stories with {sumComments} comments</h1>
+			<h1 className={styles.headlinePrimary}>Hacker Stories</h1>
 			
 			<SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit} />
 			
@@ -126,7 +172,13 @@ const App = () => {
 	);
 }
 
-const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
+type SearchFormProps = {
+	searchTerm: string;
+	onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit} : SearchFormProps) => (
 	<form onSubmit={onSearchSubmit} className={styles.searchForm}>
 		<InputWithLabel 
 			id="search" 
@@ -145,8 +197,17 @@ const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
 	</form>
 )
 
-const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, children,}) => {
-	const inputRef = React.useRef()
+type InputWithLabelProps = {
+	id: string;
+	value: string;
+	type?: string;
+	onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	isFocused?: boolean;
+	children: React.ReactNode;
+}
+
+const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, children,} : InputWithLabelProps) => {
+	const inputRef = React.useRef<HTMLInputElement>(null!)
 	React.useEffect(() => {
 		if(isFocused && inputRef.current){
 			inputRef.current.focus()
@@ -171,7 +232,8 @@ const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, child
 }
 
 
-const List = ({list, onRemoveItem}) => (
+
+const List = ({list, onRemoveItem} : ListProps) => (
 	<ul>
 		{list.map((item) => (
 			<Item
@@ -183,7 +245,7 @@ const List = ({list, onRemoveItem}) => (
 	</ul>
 )
 
-const Item = ({item, onRemoveItem}) => (
+const Item = ({item, onRemoveItem} : ItemProps) => (
 	<li className={styles.item}>
 		<span style={{ width: '40%' }}>
 			<a href={item.url}>{item.title}</a>
